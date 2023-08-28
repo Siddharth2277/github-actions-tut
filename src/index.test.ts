e52042a74084069000f2b4b45e01811c1acd9733
bluekingnfx <sid9488188099@gmail.com>
2023-08-28 19:08:30 +0530

    Added practical vitest

---
+++ src/index.test.ts
@@ -0,0 +1,12 @@
+
+import {describe,expect,it} from "vitest"
+
+describe(`Operations`,()=>{
+	it(`subtract`,()=> {
+		expect(1-1).toEqual(0)
+	})
+
+	it("add",() =>{
+		expect(1+1).toEqual(2)
+	})
+})
